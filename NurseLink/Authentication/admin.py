9a5129b0c470ef1e1150d4f344505ae05d1dba6c
Adityakuma123 <164056143+Adhikulkarn@users.noreply.github.com>
2025-08-27 08:32:23 +0530

    update 2

---
+++ NurseLink/Authentication/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import login, signup
+# Register your models here.
+
+admin.site.register(login)
+admin.site.register(signup) 
---
+++ NurseLink/Authentication/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class login(models.Model):
+    username=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+
+class signup(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.EmailField()
+    password=models.CharField(max_length=100)
+    confirmpassword=models.CharField(max_length=100)
