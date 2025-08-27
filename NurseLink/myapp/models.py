4f3d1d0a29b3f3f190b61f2c111308cf0ac22260
Adityakuma123 <164056143+Adhikulkarn@users.noreply.github.com>
2025-08-27 08:16:16 +0530

    deleted every files

--- NurseLink/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Login(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)
