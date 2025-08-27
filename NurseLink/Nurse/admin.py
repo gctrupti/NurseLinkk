1c1276bb8ee68ae58a2ff5589d32b102140d6059
Adityakuma123 <164056143+Adhikulkarn@users.noreply.github.com>
2025-08-27 08:50:41 +0530

    Installed a Nurse app to store nurse details

---
+++ NurseLink/Nurse/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Nurse_info
+# Register your models here.
+
+admin.site.register(Nurse_info)
---
+++ NurseLink/Nurse/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Nurse_info(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    phone=models.CharField(max_length=15)
+    address=models.TextField()
+    city=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+    Pin_code=models.CharField(max_length=10)
+    experience=models.IntegerField()
+    specialization=models.CharField(max_length=100)
+    availability=models.CharField(max_length=100)
+    service_type=models.CharField(max_length=100)
+    charges=models.DecimalField(max_digits=10, decimal_places=2)
+    varified=models.BooleanField(default=False)
